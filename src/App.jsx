79065f0e03231547863ef699c40ed2e596195547
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2023-02-10 20:50:41 +0530

    GraphQl+DynamoDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
-import  react , { useState } from 'react'
-
+import  react , { useState,useEffect } from 'react'
 import  {  Authenticator}  from "@aws-amplify/ui-react";
-import {Amplify} from 'aws-amplify'
+import {Amplify,API, graphqlOperation} from 'aws-amplify'
 import awsconfig from  './aws-exports'
+import '@aws-amplify/ui-react/styles.css'
+import { listTodos } from './graphql/queries'; 
+
 Amplify.configure(awsconfig)
 
 // dynamo db + Amplify set up an then lmbda 
 function App() {
+let [Todos,setTodos]=useState([])
+
+useEffect( ()=>{
+      let func =async ()=>{
+  try {
+    let list = await API.graphql(graphqlOperation(listTodos));
+
+   setTodos(list.data.listTodos.items);
+   console.log(list)
+
+  } catch (err) {
+      console.log(err)
+  }
+      }
+      
+      func()
+}
+,[])
+
+
+
 return (
-  <div>
+  <div style={{height:"100%",width:"100%",display:"flex",justifyContent:'center',alignItems:'center'}}>
     <Authenticator>
       {({ user, signOut }) => {
+        {/* console.log(user) */}
         return (
-          <div >
-         Hello Iam Premium page
+          <div>
+{
+
+Todos.map(e=>e.title)         
+}
           </div>
         );
       }}
